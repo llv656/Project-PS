cdc9bc9981444e9f3c4fe78f1a1153497eccd863
llv656 <lenin_llv@hotmail.com>
2020-08-05 13:38:12 -0500

    Se elimino carpeta duplicada

--- src/path_plugins/telegram.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-[[ $3  ]] || { exit; }
-TOKEN_TELEGRAM=$1
-CHATID_TELEGRAM=$2
-TEXT="$3"
-TIME="10"
-URL="https://api.telegram.org/bot$TOKEN_TELEGRAM/sendMessage"
-curl -s --max-time $TIME -d "chat_id=$CHATID_TELEGRAM&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
