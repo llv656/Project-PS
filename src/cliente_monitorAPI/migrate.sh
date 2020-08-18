d0f2c75f647c631c18d56371476dfead687242aa
llv656 <lenin_llv@hotmail.com>
2020-08-18 06:36:50 -0500

    Migracion del API cliente-servidor

--- src/cliente_monitorAPI/migrate.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-for var in $(ccrypt -d -c ../settings.env.cpt); do
-        export "$var"
-done
-
-python3.7 manage.py migrate
-python3.7 manage.py makemigrations
-
--- src/cliente_monitorAPI/start_clienteSERVER.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-for var in $(ccrypt -d -c ../settings.env.cpt); do
-        export "$var"
-done
-
-python3.7 manage.py runserver 0.0.0.0:9000
